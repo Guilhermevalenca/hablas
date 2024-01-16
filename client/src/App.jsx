bc243550f790d7a3b5b928244c365b2e00677d13
Guilherme Valença <guilherm3vrp@gmail.com>
2024-01-16 15:12:24 -0300

    adicionado material design icons e tailwind no front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import './assets/app.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import DefaultLayout from "./layouts/DefaultLayout.jsx";
+import './index.css';
 
 function App() {
     return (
--- client/src/layouts/DefaultLayout.jsx
+++ client/src/layouts/DefaultLayout.jsx
@@ -1,7 +1,14 @@
+import Icon from "@mdi/react";
+import {mdiAccount} from "@mdi/js";
+
 function DefaultLayout() {
     return (
         <>
-            
+           <nav className={"flex"}>
+               <button>
+                   <Icon path={mdiAccount} title={"user-profile"} /> user
+               </button>
+           </nav>
         </>
     );
 }
