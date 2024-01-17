a07438b00e186b97b527bab538e47f91fd620eef
Guilherme Valença <guilherm3vrp@gmail.com>
2024-01-16 21:38:33 -0300

    Adicionando rotas para login e register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import DefaultLayout from "./layouts/DefaultLayout.jsx";
 import socket from './plugins/socket.js';
 import HomePage from "./pages/HomePage.jsx";
+import LoginPage from './pages/Auth/LoginPage.jsx';
+import RegisterPage from './pages/Auth/RegisterPage.jsx';
 function App() {
     socket.connect();
     return (
@@ -11,7 +13,8 @@ function App() {
                 <Routes>
                     <Route path="/" element={<DefaultLayout/>}>
                         <Route index element={<HomePage />} />
-                        
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/register" element={<RegisterPage />} />
                     </Route>
                 </Routes>
             </BrowserRouter>
